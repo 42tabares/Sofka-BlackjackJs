515b8fefb5e6a63e8cccb1c000e314ffd0f9eb53
42Tabares <42tabares@gmail.com>
2022-07-12 13:16:24 -0500

    added prompt-sync lib via npm for user input handling

--- Blackjack.js
+++ Blackjack.js
@@ -1,6 +1,7 @@
 let Player = require('./player.js');
 let Card = require('./card.js');
 let Deck = require('./deck.js');
+let prompt = require('prompt-sync')();
 
 console.log("B L A C K J A C K !")
 console.log("Open the game! please input your name!")
@@ -15,3 +16,5 @@ let tabares = new Player("Tabares")
 tabares.takeStartingDeck(Mace)
 tabares.takeRandomCard(Mace)
 console.log(tabares.deck.displayDeck())
+let input = prompt(">")
+console.log(input)
