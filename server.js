c4d4a726569e251bb9d596081a2d54887fafc135
42Tabares <42tabares@gmail.com>
2022-07-13 21:53:06 -0500

    Re-organized workspace and linked index.html to server execution

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const http = require('http')
 const open = require('open')
+const fs = require('fs')
 const port = 8000
 
-
 const server = http.createServer(function(req, res){
-    res.write('Hello Web')
-    res.end()
+    res.writeHead(200, {'Content-Type' : 'text/html'})
+
+    fs.readFile('index.html', function(error, data){
+        if (error){
+            res.writeHead(404)
+            res.write('Something went wrong :c')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 server.listen(port, function(error){
     if (error){
-        console.log("ERROR: ",error)
+        console.log("ERROR AT DEPLOYMENT:\n",error)
     } else {
         console.log("Server launched on port: " + port)
         open('http:localhost:'+ port)
